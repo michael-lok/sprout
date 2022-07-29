8ca20e67e08c42857389ea11c2261ff55b6c2a29
Michael Lok <michael91lok@gmail.com>
2022-07-28 23:39:33 -0400

    alter possession and activity

--- planttracker/models.py
+++ planttracker/models.py
@@ -1,8 +1,11 @@
+import datetime as dt
+
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
-import django.core.validators as dcv
 from django.db import models
 from django.urls import reverse
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
 
 
 class Plant(models.Model):
@@ -35,12 +38,7 @@ class Possession(models.Model):
     the sprouter can indiciate the frequency in which they'd like to take action
     on the plant, such as number of days between watering/fertilizing/repotting
     """
-    sprouter = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    sprouter = models.ForeignKey(User, on_delete=models.CASCADE)
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
     nickname = models.CharField(
         max_length=100,
@@ -50,19 +48,22 @@ class Possession(models.Model):
         validators=[MinValueValidator(1)],
         blank=True,
         null=True,
-        help_text="frequency in which plant should be watered"
+        help_text="frequency in which plant should be watered",
+        verbose_name="days between watering"
     )
     days_fertilize = models.IntegerField(
         validators=[MinValueValidator(1)],
         blank=True,
         null=True,
-        help_text="frequency in which plant should be fertilized"
+        help_text="frequency in which plant should be fertilized",
+        verbose_name="days between fertilization"
     )
     days_repot = models.IntegerField(
         validators=[MinValueValidator(1)],
         blank=True,
         null=True,
-        help_text="frequency in which plant should be repotted"
+        help_text="frequency in which plant should be repotted",
+        verbose_name="days between repotting"
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -74,3 +75,27 @@ class Possession(models.Model):
         return self.nickname
 
 
+class Activity(models.Model):
+    """tracks activity conducted on plant posession"""
+
+    class ActionType(models.TextChoices):
+        ACQUIRED = "AQ", _("Acquired")
+        WATERED = "WT", _("Watered")
+        FERTILIZED = "FZ", _("Fertilized")
+        REPOTTED = "RP", _("Repotted")
+
+    posession = models.ForeignKey(Possession, on_delete=models.CASCADE)
+    action = models.CharField(
+        max_length=20,
+        choices=ActionType.choices,
+        blank=False
+    )
+    activity_date = models.DateField(
+        default=now,
+        null=False,
+        blank=False,
+        help_text="date in which activity took place."
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
