ab52c78c56b4c28b669d99ceb04dfdfab928145f
Michael Lok <michael91lok@gmail.com>
2022-07-28 22:39:19 -0400

    add caatalogconfig app

--- sprout/settings.py
+++ sprout/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # sprout apps
+    'catalog.apps.CatalogConfig'
 ]
 
 MIDDLEWARE = [
