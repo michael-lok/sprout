873349adc7422d9707d345c146699a3a6bc1adc6
Michael Lok <michael91lok@gmail.com>
2022-07-28 22:31:02 -0400

    __str__ reference for plant

--- catalog/models.py
+++ catalog/models.py
@@ -26,3 +26,6 @@ class Plant(models.model):
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.common_name} ({self.scientific_name})"
