30e12eeb53514ec1d50e71b4f0b55bda5c6e4c8e
Michael Lok <michael91lok@gmail.com>
2022-07-29 20:07:26 -0400

    introduce planttracker homepage

--- planttracker/views.py
+++ planttracker/views.py
@@ -1,3 +1,35 @@
+from django.db.models import Count
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import (
+    Plant,
+    Possession,
+    Activity
+)
+
+
+def index(request):
+    """view function for home page of site."""
+    num_plants = Plant.objects.all().count()
+    num_sprouters = Possession.objects.all().annotate(
+        Count("sprouter", distinct=True)
+    )
+    num_possessions = Possession.objects.all().count()
+    num_activity = Activity.objects.all().count()
+
+    return render(
+        request,
+        "index.html",
+        context={
+            "num_plants": num_plants,
+            "num_sprouters": num_sprouters,
+            "num_possessions": num_possessions,
+            "num_activity": num_activity
+        }
+    )
+
+
+class PlantView(generic.ListView):
+    model = Plant
+    paginate_by = 10
